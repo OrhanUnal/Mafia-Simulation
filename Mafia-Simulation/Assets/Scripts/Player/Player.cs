99e1b28c18ba4288643ee10af3beba46c6e76c24
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2026-02-18 14:51:56 +0300

    hnp-6 and hnp-9

--- Mafia-Simulation/Assets/Scripts/Player/Player.cs
+++ Mafia-Simulation/Assets/Scripts/Player/Player.cs
@@ -6,53 +6,44 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float speed = 3.0f, maxInteractDistance = 10f, drag = 30f, maxSpeed = 100f, jumpSpeed = 1f, gravityForPlayer = -9.81f, sensivity = 1f;
-
-    [SerializeField]
-    AnimationCurve jumpCurve;
+    float acceleration = 70f, maxInteractDistance = 10f, drag = 25f, maxSpeed = 4, gravityForPlayer = -9.81f, sensivity = 1f;
 
     [SerializeField]
     Transform cameraPickUpPosition, head;
 
     private CharacterController _characterController;
     private PlayerInput _playerInput;
+    private Vector3 _velocity;
     private float _pitch;
     private bool _canMove, _holdingSomething;
     private GameObject _grabbedObject;
 
 
     //inputs
-    private InputAction _mouseInput, _interact, _walk, _jump, _pickUpDropDown;
+    private InputAction _mouseInput, _interact, _walk, _pickUpDropDown;
 
     //variables for movement
     private Vector3 _move;
-    private bool _inJump;
-    private float _elapsedJumpTime;
-    private float _velocityAtJumpStart;
 
     void Awake()
     {
+        _velocity = Vector3.zero;
         _pitch = 0;
-        _elapsedJumpTime = 0;
-        _inJump = false;
         _holdingSomething = false;
         _characterController = GetComponent<CharacterController>();
         _playerInput = GetComponent<PlayerInput>();
-        //_interact = _playerInput.actions["Interract"];
+        _interact = _playerInput.actions["Interact"];
         _walk = _playerInput.actions["Move"];
-        _jump = _playerInput.actions["Jump"];
         _mouseInput = _playerInput.actions["Look"];
-        //_pickUpDropDown = _playerInput.actions["PickUp DropDown"];
+        _pickUpDropDown = _playerInput.actions["PickUp DropDown"];
     }
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        //_interact.performed += InteractSomething;
-        //_pickUpDropDown.performed += PickUpAndDropDownSomething;
-        _jump.started += JumpLogic;
-        _jump.canceled += JumpLogic;
+        _interact.performed += InteractSomething;
+        _pickUpDropDown.performed += PickUpAndDropDownSomething;
     }
     void FixedUpdate()
     {
@@ -72,42 +63,28 @@ public class Player : MonoBehaviour
 
     private void MovementLogic()
     {
-        if (_inJump && _elapsedJumpTime < 0.4f)
-        {
-            _elapsedJumpTime += Time.deltaTime;
-            _move.y = Mathf.Lerp(_velocityAtJumpStart, jumpSpeed, jumpCurve.Evaluate(_elapsedJumpTime));
-        }
-        _move.x = _walk.ReadValue<Vector2>().x;
-        _move.z = _walk.ReadValue<Vector2>().y;
-        _move = transform.rotation * _move;
-        Vector3 newVelocity = _characterController.velocity + _move * speed;
-        Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
-        newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
-        newVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
-        _characterController.Move(newVelocity * Time.deltaTime);
+        Vector2 input = _walk.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(input.x, 0, input.y);
+        moveDirection = transform.TransformDirection(moveDirection);
 
+        _velocity.x += moveDirection.x * acceleration * Time.deltaTime;
+        _velocity.z += moveDirection.z * acceleration * Time.deltaTime;
+
+        Vector3 horizontalVelocity = new Vector3(_velocity.x, 0, _velocity.z);
+        Vector3 dragForce = horizontalVelocity.normalized * drag * Time.deltaTime;
+        horizontalVelocity = (horizontalVelocity.magnitude - dragForce.magnitude > 0) ? horizontalVelocity - dragForce : Vector3.zero;
+        horizontalVelocity = Vector3.ClampMagnitude(horizontalVelocity, maxSpeed);
+
+        _velocity.x = horizontalVelocity.x;
+        _velocity.z = horizontalVelocity.z;
+        _characterController.Move(_velocity * Time.deltaTime);
     }
 
     private void GravityLogic()
     {
-        if (!_characterController.isGrounded)
-            _move.y += gravityForPlayer * Time.deltaTime;
-        else _move.y = -0.2f;
-    }
-
-    private void JumpLogic(InputAction.CallbackContext context)
-    {
-        if (context.canceled)
-        {
-            _inJump = false;
-            _move.y = 0;
-            _elapsedJumpTime = 0;
-        }
-        else
-        {
-            _velocityAtJumpStart = _characterController.velocity.y;
-            _inJump = true;
-        }
+        if (_characterController.isGrounded)
+            _velocity.y = -2f;
+        else _velocity.y += gravityForPlayer * Time.deltaTime;
     }
 
     private void InteractSomething(InputAction.CallbackContext context)
@@ -117,7 +94,10 @@ public class Player : MonoBehaviour
         {
             interactableObject = _grabbedObject.GetComponent<IInteractable>();
             if (interactableObject != null)
+            {
                 interactableObject.InteractLogic();
+                return;
+            }
         }
         else
         {
@@ -133,6 +113,14 @@ public class Player : MonoBehaviour
 
     private void PickUpAndDropDownSomething(InputAction.CallbackContext context)
     {
+        if (_holdingSomething)
+        {
+            _holdingSomething = false;
+            IGraspable grasped = _grabbedObject.GetComponent<IGraspable>();
+            grasped.PickOrDropObject(cameraPickUpPosition);
+            _grabbedObject = null;
+            return;
+        }
         RaycastHit rayInfo = ShootRayFromCamera();
         if (rayInfo.collider != null)
         {
@@ -147,11 +135,11 @@ public class Player : MonoBehaviour
 
     private RaycastHit ShootRayFromCamera()
     {
-
+        Debug.DrawRay(head.position, head.forward * maxInteractDistance, Color.red, 0.1f);
         Ray rayOfInteract = new Ray
         {
-            origin = head.transform.position,
-            direction = transform.forward,
+            origin = head.position,
+            direction = head.forward,
         };
         RaycastHit hitInfo;
         Physics.Raycast(rayOfInteract, out hitInfo, maxInteractDistance);
